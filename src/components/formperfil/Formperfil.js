2aa2734ed84b553e2aa7b0326b7d1e2b46b36e39
VivianaMurillom <viviana.zs104@gmail.com>
2022-06-28 09:23:10 -0500

    The page to modify the profile and the form component are created.

---
+++ src/components/formperfil/Formperfil.js
@@ -0,0 +1,67 @@
+let Formperfil=()=>{
+    return(
+        <>
+        <section>
+            <h2>Editar datos</h2>
+
+            <form>
+                <div>
+                    <label>Nombres</label>
+                    <input
+                    type="text"/>
+                </div>
+                <div>
+                    <label>Apellidos</label>
+                    <input
+                    type="text"/>
+                </div>
+                <div>
+                    <label>Tipo de documento de identidad</label>
+                    <select>
+                        <option value="" selected>Elija su documento</option>
+                        <option value="Registro Civil" selected>Registro Civil</option>
+                        <option value="Tarjeta de Identidad" selected>Tarjeta de Identidad</option>
+                        <option value="Cédula de ciudadanía" selected>Cédula de ciudadanía</option>
+                        <option value="Cédula de extranjería" selected>Cédula de extranjería</option>
+                    </select>
+                </div>
+                <div>
+                    <label>Número de documento de identidad</label>
+                    <input
+                    type="number"/>
+                </div>
+                <div>
+                    <label>Fecha de nacimiento</label>
+                    <input
+                    type="date"/>
+                </div>
+                <div>
+                    <label>Genero</label>
+                    <input
+                    type="text"/>
+                </div>
+                <div>
+                    <label>Correo</label>
+                    <input
+                    type="mail"/>
+                </div>
+                <div>
+                    <label>Telefono de contacto</label>
+                    <input
+                    type="number"/>
+                </div>
+                <div>
+                    <label>País de origen</label>
+                    <input
+                    type="text"/>
+                </div>
+                <div>
+                    <button type="submit">Guardar cambios</button>
+                </div>
+            </form>
+        </section>
+        </>
+    )
+}
+
+export default Formperfil;
---
+++ src/pages/editarperfil/EditarPerfil.js
@@ -0,0 +1,15 @@
+import Formperfil from "../../components/formperfil/Formperfil";
+
+let EditarPerfil=()=>{
+    return(
+        <>
+        <main>
+            <h1>Perfil</h1>
+            <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.</p>
+            <Formperfil/>
+        </main>
+        </>
+    )
+}
+
+export default EditarPerfil;
