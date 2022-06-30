06971c80ab06a6a01b9a525ce34452d65098cc32
VivianaMurillom <viviana.zs104@gmail.com>
2022-06-30 07:22:21 -0500

    Temporary changes are made to the cambio de foto design to study functional implementation.

--- src/pages/cambiarfoto/CambiarFoto.js
+++ src/pages/cambiarfoto/CambiarFoto.js
@@ -8,9 +8,9 @@ let CambiarFoto=()=>{
 
     // Constantes del componente primer modal bootstrap
 
-    const [showM1, setShowM1] = useState(false);
-    const handleCloseM1 = () => setShowM1(false);
-    const handleShowM1 = () => setShowM1(true);
+    // const [showM1, setShowM1] = useState(false);
+    // const handleCloseM1 = () => setShowM1(false);
+    // const handleShowM1 = () => setShowM1(true);
 
     // Constantes del componente segundo modal bootstrap
     const [show, setShow] = useState(false);
@@ -27,15 +27,15 @@ let CambiarFoto=()=>{
             <img src={noImagen} alt="No imagen"/>
 
             <div>
-                <Button variant="primary" onClick={handleShowM1}>
-                    Tomar foto
-                </Button>
+                {/* <Button variant="primary" onClick={handleShowM1}>
+                  Tomar foto
+                </Button> */}
                 <Button variant="primary" onClick={handleShow}>
-        Subir foto
-      </Button>
+                  Subir foto
+                </Button>
             </div>
 
-            <Modal show={showM1} onHide={handleCloseM1}>
+            {/* <Modal show={showM1} onHide={handleCloseM1}>
                 <Modal.Header closeButton>
                     <Modal.Title>Modal heading</Modal.Title>
                 </Modal.Header>
@@ -48,22 +48,22 @@ let CambiarFoto=()=>{
                     Save Changes
                     </Button>
                 </Modal.Footer>
-            </Modal>
+            </Modal> */}
 
             <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
+              <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                  Save Changes
+                </Button>
+              </Modal.Footer>
+            </Modal>
 
         </section>
 
