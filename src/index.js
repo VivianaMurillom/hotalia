ead44d4c1f66f3fcd3c2dd028e730c0be9064835
VivianaMurillom <viviana.zs104@gmail.com>
2022-07-06 16:22:33 -0500

    Creation of reservaciones page

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import EditarContrasena from './pages/editarcont/EditarContrasena';
 import CambiarFoto from './pages/cambiarfoto/CambiarFoto';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Reservaciones from './pages/reservaciones/Reservaciones';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -17,6 +19,8 @@ root.render(
         <Route path="/editar-perfil" element={<EditarPerfil />} />
         <Route path="/editar-contrasena" element={<EditarContrasena />} />
         <Route path="/cambiar-foto" element={<CambiarFoto />} />
+
+        <Route path="/reservaciones" element={<Reservaciones />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
