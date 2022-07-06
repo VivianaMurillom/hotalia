c23a9bfd997e13dd5f31cf712dcdf8fb112daa37
VivianaMurillom <viviana.zs104@gmail.com>
2022-07-06 08:56:03 -0500

    The room search form component is created.

--- src/components/formbushabita/FBHabitacion.js
+++ src/components/formbushabita/FBHabitacion.js
@@ -4,48 +4,50 @@ let FBHabitacion=()=>{
     return(
         <>
             <form className="form-search-room">
-                <div className="form-sections-room">
-                    <div className="form-divisions">
-                        <label>Fecha de entrada</label>
-                        <input type="date"/>
+                <div className="form-room-search">
+                    <div className="form-sections-room">
+                        <div className="form-divisions">
+                            <label>Fecha de entrada</label>
+                            <input type="date"/>
+                        </div>
+                        <div className="form-divisions">
+                            <label>Fecha de salida</label>
+                            <input type="date"/>
+                        </div>
                     </div>
-                    <div className="form-divisions">
-                        <label>Fecha de salida</label>
-                        <input type="date"/>
-                    </div>
-                </div>
-                <div className="form-sections-room">
-                    <div className="form-divisions">
-                        <label>Cantidad de personas adultas</label>
-                        <select>
-                        <option value="" selected>Elija la cantidad</option>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                                <option value="9">9</option>
-                                <option value="10">10</option>
-                        </select>
-                    </div>
-                    <div className="form-divisions">
-                        <label>Cantidad de personas niños</label>
-                        <select>
-                        <option value="" selected>Elija la cantidad</option>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                                <option value="9">9</option>
-                                <option value="10">10</option>
-                        </select>
+                    <div className="form-sections-room">
+                        <div className="form-divisions">
+                            <label>Cantidad de personas adultas</label>
+                            <select>
+                            <option value="" selected>Elija la cantidad</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                            </select>
+                        </div>
+                        <div className="form-divisions">
+                            <label>Cantidad de personas niños</label>
+                            <select>
+                            <option value="" selected>Elija la cantidad</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                            </select>
+                        </div>
                     </div>
                 </div>
                 <div className="form-rooms-button">
