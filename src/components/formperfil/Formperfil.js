1209ea4684f84e9b81ade720547c982cdd4bf9ee
VivianaMurillom <viviana.zs104@gmail.com>
2022-07-12 17:50:31 -0500

    Adding field validation with Formik

--- src/components/formperfil/Formperfil.js
+++ src/components/formperfil/Formperfil.js
@@ -1,50 +1,87 @@
 import "./Formperfil.css";
 import Swal from "sweetalert2";
+import {Formik, Form, Field, ErrorMessage} from "formik";
 
 let Formperfil=()=>{
-
-    const sendFormProfile=(e)=>{
-      e.preventDefault();  
-
-      Swal.fire(
-        'Información actualizada',
-        'Su información de perfil ha sido actualizada correctamente!',
-        'success'
-      )
-    }
 
     return(
         <>
         <section className="form-modify-profile">
             <h3>Editar datos de contacto</h3>
 
-            <form>
+            <Formik
+            
+            initialValues={{correo:'', telefono:''}}
+            
+            validate={(valores)=>{
+                let errores={}
+
+                // Validación de errores input correo
+                if (!valores.correo) {
+                    errores.correo = 'Por favor ingresar su correo electrónico'
+                } else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.correo)){
+                    errores.correo = 'Por favor ingresar un correo válido, e.j: usuario@mail.com'
+                }
+
+                // Validación de errores input telefono
+                if (!valores.telefono) {
+                    errores.telefono = 'Por favor ingresar su número telefónico de contacto'
+                } else if (!/^\d{7,14}$/.test(valores.telefono)){
+                    errores.telefono = 'El teléfono solo puede contener numeros y mínimo 7 y máximo 14 dígitos'
+                }
+
+                return errores;
+            }}
+
+            onSubmit={(valores,{resetForm})=>{
+                    
+                resetForm();
+
+                Swal.fire(
+                    'Información actualizada',
+                    'Su información de perfil ha sido actualizada correctamente!',
+                    'success'
+                  )
+            }}
+            >
+
+            {({errors})=>(
+                <Form>
                 <div className="form-sections-profile">
                     <div className="form-divisions">
                         <label>Correo</label>
-                        <input
+                        <Field
                         className=""
-                        type="mail"/>
+                        type="email"
+                        name="correo"
+                        id="correo"/>
                         <i class="fa-solid fa-circle-exclamation input-i"></i>
-                        <div className="mensaje-error-contact">
-                            <p><i class="fa-solid fa-triangle-exclamation"></i> Por favor ingresar un correo válido, e.j: usuario@mail.com</p>
-                        </div>
+                        <ErrorMessage name='correo' component={()=>(
+                            <div className='mensaje-error-contact'><i class="fa-solid fa-triangle-exclamation"></i> {errors.correo}</div>
+                        )}/>
                     </div>
                     <div className="form-divisions">
                         <label>Telefono de contacto</label>
-                        <input
+                        <Field
                         className=""
-                        type="number"/>
+                        type="number"
+                        name="telefono"
+                        id="telefono"/>
                         <i class="fa-solid fa-circle-exclamation input-i"></i>
-                        <div className="mensaje-error-contact">
-                            <p><i class="fa-solid fa-triangle-exclamation"></i> El teléfono solo puede contener numeros y mínimo 7 y máximo 14 dígitos</p>
-                        </div>
+                        <ErrorMessage name='telefono' component={()=>(
+                            <div className='mensaje-error-contact'><i class="fa-solid fa-triangle-exclamation"></i> {errors.telefono}</div>
+                        )}/>
                     </div>
                 </div>
                 <div className="form-profile-button">
-                    <button type="submit" className="general-button" onClick={sendFormProfile}>Guardar cambios</button>
+                    <button type="submit" className="general-button">Guardar cambios</button>
                 </div>
-            </form>
+            </Form>
+            )}
+
+            
+
+            </Formik>
         </section>
         </>
     )
