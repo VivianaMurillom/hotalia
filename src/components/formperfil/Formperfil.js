fda0e4b592690f04156e85388d12f023cd77607c
VivianaMurillom <viviana.zs104@gmail.com>
2022-07-12 10:36:24 -0500

    Adding styles for field validation

--- src/components/formperfil/Formperfil.js
+++ src/components/formperfil/Formperfil.js
@@ -23,12 +23,22 @@ let Formperfil=()=>{
                     <div className="form-divisions">
                         <label>Correo</label>
                         <input
+                        className=""
                         type="mail"/>
+                        <i class="fa-solid fa-circle-exclamation input-i"></i>
+                        <div className="mensaje-error-contact">
+                            <p><i class="fa-solid fa-triangle-exclamation"></i> Por favor ingresar un correo válido, e.j: usuario@mail.com</p>
+                        </div>
                     </div>
                     <div className="form-divisions">
                         <label>Telefono de contacto</label>
                         <input
+                        className=""
                         type="number"/>
+                        <i class="fa-solid fa-circle-exclamation input-i"></i>
+                        <div className="mensaje-error-contact">
+                            <p><i class="fa-solid fa-triangle-exclamation"></i> El teléfono solo puede contener numeros y mínimo 7 y máximo 14 dígitos</p>
+                        </div>
                     </div>
                 </div>
                 <div className="form-profile-button">
