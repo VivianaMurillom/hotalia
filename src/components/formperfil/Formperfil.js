28d364ac3cd2bac11551c9d4c7d448bc8970b051
VivianaMurillom <viviana.zs104@gmail.com>
2022-07-14 21:27:03 -0500

    Design details are finalized for field validation of the contact data change form.

--- src/components/formperfil/Formperfil.js
+++ src/components/formperfil/Formperfil.js
@@ -1,87 +1,84 @@
 import "./Formperfil.css";
 import Swal from "sweetalert2";
-import {Formik, Form, Field, ErrorMessage} from "formik";
+import Input from "../../components/inputsforms/Input";
+import {useState} from "react";
+import {MensajeError} from "../../elements/Formularios";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faExclamationTriangle} from '@fortawesome/free-solid-svg-icons';
 
 let Formperfil=()=>{
 
+    const [correo, cambiarCorreo] = useState({campo: '', valido: null});
+    const [telefono, cambiarTelefono] = useState({campo: '', valido: null});
+    const [formularioValido,cambiarFormularioValido]= useState(null);
+
+    const expresiones = {
+		correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+		telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+	}
+
+    const actualizarDatos=(e)=>{
+        e.preventDefault();
+
+        if (
+            correo.valido === 'true' &&
+		    telefono.valido === 'true') {
+
+            cambiarCorreo({campo: '', valido: null});
+            cambiarTelefono({campo: '', valido: null});
+            cambiarFormularioValido(true);
+
+            Swal.fire(
+            'Información actualizada',
+            'Su información de perfil ha sido actualizada correctamente!',
+            'success'
+            )
+
+        } else{
+            cambiarFormularioValido(false);
+        }
+    }
+    
     return(
         <>
         <section className="form-modify-profile">
             <h3>Editar datos de contacto</h3>
 
-            <Formik
-            
-            initialValues={{correo:'', telefono:''}}
-            
-            validate={(valores)=>{
-                let errores={}
-
-                // Validación de errores input correo
-                if (!valores.correo) {
-                    errores.correo = 'Por favor ingresar su correo electrónico'
-                } else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.correo)){
-                    errores.correo = 'Por favor ingresar un correo válido, e.j: usuario@mail.com'
-                }
-
-                // Validación de errores input telefono
-                if (!valores.telefono) {
-                    errores.telefono = 'Por favor ingresar su número telefónico de contacto'
-                } else if (!/^\d{7,14}$/.test(valores.telefono)){
-                    errores.telefono = 'El teléfono solo puede contener numeros y mínimo 7 y máximo 14 dígitos'
-                }
-
-                return errores;
-            }}
-
-            onSubmit={(valores,{resetForm})=>{
-                    
-                resetForm();
-
-                Swal.fire(
-                    'Información actualizada',
-                    'Su información de perfil ha sido actualizada correctamente!',
-                    'success'
-                  )
-            }}
-            >
-
-            {({errors})=>(
-                <Form>
+                <form action="" onSubmit={actualizarDatos}>
                 <div className="form-sections-profile">
-                    <div className="form-divisions">
-                        <label htmlFor="correo">Correo</label>
-                        <Field
-                        className=""
-                        type="email"
+                    <div className="form-sections-profile">
+                        <Input
+                        label="Correo electrónico"
+                        tipo="email"
                         name="correo"
-                        id="correo"/>
-                        <i class="fa-solid fa-circle-exclamation input-i"></i>
-                        <ErrorMessage name='correo' component={()=>(
-                            <div className='mensaje-error-contact'><i class="fa-solid fa-triangle-exclamation"></i> {errors.correo}</div>
-                        )}/>
+                        estado={correo}
+                        cambiarEstado={cambiarCorreo}
+                        expresionRegular={expresiones.correo}
+                        leyendaError="El correo debe cumplir las características requeridas, ej: mail@mail.com"/>
                     </div>
-                    <div className="form-divisions">
-                        <label htmlFor="telefono">Telefono de contacto</label>
-                        <Field
-                        className=""
-                        type="number"
+                    <div className="form-sections-profile">
+                        <Input
+                        label="Télefono de contacto"
+                        tipo="number"
                         name="telefono"
-                        id="telefono"/>
-                        <i class="fa-solid fa-circle-exclamation input-i"></i>
-                        <ErrorMessage name='telefono' component={()=>(
-                            <div className='mensaje-error-contact'><i class="fa-solid fa-triangle-exclamation"></i> {errors.telefono}</div>
-                        )}/>
+                        estado={telefono}
+                        cambiarEstado={cambiarTelefono}
+                        expresionRegular={expresiones.telefono}
+                        leyendaError="El telefono solo puede contener numeros entre 7 y 14 dígitos."
+                        />
+
                     </div>
                 </div>
+                {formularioValido===false && <MensajeError>
+                <p>
+                <FontAwesomeIcon icon={faExclamationTriangle}/>
+                <b>Error:</b> Por favor diligenciar el formulario correctamente.</p>
+                </MensajeError>}
                 <div className="form-profile-button">
                     <button type="submit" className="general-button">Guardar cambios</button>
                 </div>
-            </Form>
-            )}
-
-            
-
-            </Formik>
+            </form>
+        
         </section>
         </>
     )
